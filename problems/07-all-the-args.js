8d2ac0e9154132b28d0c68ff6f58af63d71a304a
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-13 19:08:18 -0700

    pass tests

--- problems/07-all-the-args.js
+++ problems/07-all-the-args.js
@@ -1,6 +1,29 @@
 function allTheArgs(func, ...args) {
-  // Your code here
+  return (...moreArgs) => func.apply(this, [...args, ...moreArgs]);
 }
+/*
+function sum(...nums) {
+  return nums.reduce((acc, num) => acc + num);
+}
+
+const onePlusTwo = allTheArgs(sum, 1, 2);
+console.log('🚀 ~ file: index.js ~ line 6 ~ onePlusTwo', onePlusTwo);
+
+const onePlusTwoPlusThree = onePlusTwo(3);
+const onePlusTwoPlusFour = onePlusTwo(4);
+
+console.log(onePlusTwoPlusThree); // => 6
+console.log(onePlusTwoPlusFour); // => 7
+
+function allTheArgs(func, ...args) {
+  console.log(args);
+  // debugger;
+  // Your code here
+  return (...moreArgs) => func.apply(this, [...args, ...moreArgs]);
+}
+
+
+*/
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
