889cad9b48e7f46503f96c14524f5d26e164cd94
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-13 18:55:30 -0700

    pass tests

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,10 +1,30 @@
 // Your code here
+const Calculator = require('./02-calculator.js');
+
+class FancyCalculator extends Calculator {
+  constructor() {
+    super();
+  }
+
+  setTotal(newTotal) {
+    this.total = newTotal;
+    return this.total;
+  }
+  modulo(num) {
+    this.total = this.total % num;
+    return this.total;
+  }
+  squared() {
+    this.total *= this.total;
+    return this.total;
+  }
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = FancyCalculator;
+  module.exports = FancyCalculator;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
